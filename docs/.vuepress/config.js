8beb6cb86a6dc556356b1fae98176a9e58086721
qiaoqzhang <qiaoqzhang@tencent.com>
2022-06-21 21:06:27 +0800

    feat: 个人站点发布

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  base: '/firefox-ui',
+  title: 'Firefox-ui',
+  base: '/firefox-ui/',
   themeConfig: {
+    nav: [
+      {
+        text: 'home',
+        link: '/'
+      },{
+        text: 'Githup',
+        link: 'https://github.com/qiaoQ/firefox-ui/tree/master'
+      },
+      {
+        text: 'Npm',
+        link: 'https://www.npmjs.com/package/firefox-ui'
+      }
+    ],
     sidebar: [
       '/',
       '/componentDocs/card'
